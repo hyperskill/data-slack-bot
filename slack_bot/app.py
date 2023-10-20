11128ec6ed98e2fdf5c0acde2e16c70d7a8eecbd
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2023-10-20 21:20:28 +0400

    starting to implement interactive chat

--- slack_bot/app.py
+++ slack_bot/app.py
@@ -2,18 +2,18 @@ from __future__ import annotations
 
 import logging
 import os
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from data_assistant.scenarios import submit_an_issue
 
 if TYPE_CHECKING:
     from slack_bolt.context.say import Say
-    from slack_sdk.web import WebClient
 
 
-load_dotenv(".vars")
+load_dotenv()
 logger = logging.getLogger(__name__)
 
 
@@ -24,70 +24,13 @@ SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
 app = App(token=SLACK_BOT_TOKEN)
 
 
-# Add functionality here
-@app.event("app_home_opened")
-def update_home_tab(
-    client: WebClient, event: dict[str, Any], logger: logging.Logger
-) -> None:
-    try:
-        # views.publish is the method that your app uses to push a view to the Home tab
-        client.views_publish(
-            # the user that opened your app's app home
-            user_id=event["user"],
-            # the view object that appears in the app home
-            view={
-                "type": "home",
-                "callback_id": "home_view",
-                # body of the view
-                "blocks": [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*Welcome to your _App's Home_* :tada:",
-                        },
-                    },
-                    {"type": "divider"},
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "This button won't do much for now but you can set "
-                            "up a listener for it using the `actions()` method "
-                            "and passing its unique `action_id`. See an "
-                            "example in the `examples` folder within your Bolt app.",
-                        },
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "button",
-                                "text": {"type": "plain_text", "text": "Click me!"},
-                            }
-                        ],
-                    },
-                ],
-            },
-        )
-
-    except Exception:
-        logger.exception("Error publishing home tab")
-
-
-@app.event("app_mention")
-def event_test(body: str, say: Say, logger: logging.Logger) -> None:
-    try:
-        say("What's up?")
-        logger.info(body)
-    except Exception:
-        logger.exception("Error responding to mention")
-
-
 @app.event("message")
 def event_test2(body: str, say: Say, logger: logging.Logger) -> None:
     try:
-        say("What's up?")
+        message = body["event"]["text"]
+        reply = str(submit_an_issue(message))
+        say("I am thinking...")
+        say(reply)
         logger.info(body)
     except Exception:
         logger.exception("Error responding to mention")
