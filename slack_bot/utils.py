d994cd3b7991a939c5044d8fc2a956bd1492fefd
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2023-10-22 12:50:53 +0400

    fix none return in clean_message_text

--- slack_bot/utils.py
+++ slack_bot/utils.py
@@ -29,7 +29,6 @@ You will answer the question as truthfully as possible.
 If you're unsure of the answer, say Sorry, I don't know.
 """
 WAIT_MESSAGE = "Got your request. Please wait."
-N_CHUNKS_TO_CONCAT_BEFORE_UPDATING = 20
 MAX_TOKENS = 8192
 
 
@@ -38,11 +37,13 @@ def extract_url_list(text: str) -> list[str] | None:
         r"<(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)>"
     )
     url_list = url_pattern.findall(text)
+
     return url_list if len(url_list) > 0 else None
 
 
 def augment_user_message(user_message: str, url_list: list[str]) -> str:
     all_url_content = ""
+
     for url in url_list:
         downloaded = fetch_url(url)
         url_content = extract(downloaded, config=newconfig)
@@ -50,6 +51,7 @@ def augment_user_message(user_message: str, url_list: list[str]) -> str:
         all_url_content = (
             all_url_content + f' Contents of {url} : \n """ {url_content} """'
         )
+
     return user_message + "\n" + all_url_content
 
 
@@ -63,23 +65,29 @@ def num_tokens_from_messages(
     except KeyError:
         print("Warning: model not found. Using cl100k_base encoding.")  # noqa: T201
         encoding = tiktoken.get_encoding("cl100k_base")
+
     if model == "gpt-3.5-turbo":
         print(  # noqa: T201
             "Warning: gpt-3.5-turbo may change over time. "
             "Returning num tokens assuming gpt-3.5-turbo-0301."
         )
+
         return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+
     elif model == "gpt-4":  # noqa: RET505
         print(  # noqa: T201
             "Warning: gpt-4 may change over time."
             "Returning num tokens assuming gpt-4-0314."
         )
+
         return num_tokens_from_messages(messages, model="gpt-4-0314")
+
     elif model == "gpt-3.5-turbo-0301":
         tokens_per_message = (
             4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
         )
         tokens_per_name = -1  # if there's a name, the role is omitted
+
     elif model == "gpt-4-0314":
         tokens_per_message = 3
         tokens_per_name = 1
@@ -89,43 +97,55 @@ def num_tokens_from_messages(
             See https://github.com/openai/openai-python/blob/main/chatml.md
             for information on how messages are converted to tokens."""
         )
+
     num_tokens = 0
+
     for message in messages:
         num_tokens += tokens_per_message
+
         for key, value in message.items():
             num_tokens += len(encoding.encode(value))
+
             if key == "name":
                 num_tokens += tokens_per_name
+
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+
     return num_tokens
+
+
+def clean_message_text(message_text: str, role: str, bot_user_id: str) -> str:
+    if (f"<@{bot_user_id}>" in message_text) or (role == "assistant"):
+        message_text = message_text.replace(f"<@{bot_user_id}>", "").strip()
+
+    return message_text.strip()
+
+
+def process_message(message: dict[str, str], bot_user_id: str, role: str) -> str:
+    message_text = message["text"]
+
+    if role == "user":
+        url_list = extract_url_list(message_text)
+
+        if url_list:
+            message_text = augment_user_message(message_text, url_list)
+
+    return clean_message_text(message_text, role, bot_user_id)
 
 
 def process_conversation_history(
     conversation_history: SlackResponse, bot_user_id: str
 ) -> list[dict[str, str]]:
     messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+
     for message in conversation_history["messages"][:-1]:
         role = "assistant" if message["user"] == bot_user_id else "user"
-        message_text = process_message(message, bot_user_id)
+        message_text = process_message(message, bot_user_id, role)
+
         if message_text:
             messages.append({"role": role, "content": message_text})
+
     return messages
-
-
-def process_message(message: dict[str, str], bot_user_id: str) -> Any | None:
-    message_text = message["text"]
-    role = "assistant" if message["user"] == bot_user_id else "user"
-    if role == "user":
-        url_list = extract_url_list(message_text)
-        if url_list:
-            message_text = augment_user_message(message_text, url_list)
-    return clean_message_text(message_text, role, bot_user_id)
-
-
-def clean_message_text(message_text: str, role: str, bot_user_id: str) -> Any | None:
-    if (f"<@{bot_user_id}>" in message_text) or (role == "assistant"):
-        return message_text.replace(f"<@{bot_user_id}>", "").strip()
-    return None
 
 
 def get_conversation_history(
@@ -143,10 +163,12 @@ def make_ai_response(
         channel_id = body["event"]["channel"]
         thread_ts = body["event"].get("thread_ts", body["event"]["ts"])
         bot_user_id = context["bot_user_id"]
+
         slack_resp = app.client.chat_postMessage(
             channel=channel_id, thread_ts=thread_ts, text=WAIT_MESSAGE
         )
         reply_message_ts = slack_resp["message"]["ts"]
+
         conversation_history = get_conversation_history(app, channel_id, thread_ts)
         messages = process_conversation_history(conversation_history, bot_user_id)
         num_tokens = num_tokens_from_messages(messages)
