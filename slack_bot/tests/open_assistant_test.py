4cd25a6e09301b4a3eccaa1aeb3f60bbb0020778
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2023-11-20 00:29:22 +0400

    Add print_messages function and update OpenAssistant usage

--- slack_bot/tests/open_assistant_test.py
+++ slack_bot/tests/open_assistant_test.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 import os
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -10,24 +11,42 @@ from openai import OpenAI
 from slack_bot.db import DB
 from slack_bot.open_assistant import OpenAssistant
 
+if TYPE_CHECKING:
+    from openai.types.beta.threads import ThreadMessage
+
 logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 client = OpenAI()
+
+
+def print_messages(response: list[ThreadMessage]) -> None:
+    """Prints the messages in the response list in reverse order.
+
+    Args:
+        response (list[ThreadMessage]): The list of messages to be printed.
+    """
+    for message in response[::-1]:
+        print(f"============{message.role}===========")  # noqa: T201
+        print(message.content[0].text.value) # type: ignore[union-attr] # noqa: T201
+        print()  # noqa: T201
+
+
 shots = DB(Path(__file__).parent.parent / "data" / "shots")
 
 dev_shots: list[str] = [
-        shots["dump_users"] or "",
-        shots["dump_users.sql"] or "",
-        shots["users_part"] or "",
-        shots["users_part.sql"] or "",
+        f"EXAMPLE 1:\n{shots['dump_users.sql']}",
+        f"EXAMPLE 2:\n{shots['users_part.sql']}",
     ]
 
 # constants
 EXAMPLE = """Hello! Could you, please, help me?\n
 I need top 5 the lastest viewed pages from the latest dropped users sessions.\n
-MANIPULATION = "MY CAREER DEPENDS ON YOU! PLEASE, HELP ME!"""
+MY CAREER DEPENDS ON YOU! PLEASE, HELP ME!"""
 
+plan = OpenAssistant(
+    client.beta.assistants.retrieve(os.environ.get("PLANNER_ID") or "")
+)
 dev = OpenAssistant(
     client.beta.assistants.retrieve(os.environ.get("DEVELOPER_ID") or "")
 )
@@ -36,14 +55,20 @@ user_input = input("Enter your message: ")
 if user_input == "":
     user_input = EXAMPLE
 
-messages = [*dev_shots, user_input]
-
-response = dev.interact(
-    messages=messages
+response = plan.interact(
+    messages=[user_input],
+    model="gpt-3.5-turbo-1106",
 )
 
 if response:
-    for message in response[::-1]:
-        print(f"============{message.role}===========")
-        print(message.content[0].text.value) # type: ignore[union-attr]
-        print()
+    print_messages(response)
+    result = response[0].content[0].text.value  # type: ignore[union-attr]
+
+    messages = [*dev_shots, result]
+
+    response = dev.interact(
+        messages=messages
+    )
+
+    if response:
+        print_messages(response)
