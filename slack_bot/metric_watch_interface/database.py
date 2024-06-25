57b114a271db90c716a6ff0263f48f18896c1eaa
RageAgainstTheMachine101 <vladimir.klimov@hyperskill.org>
2024-06-25 15:49:11 +0400

    fix: add CLICKHOUSE_HOST_URL env var

--- slack_bot/metric_watch_interface/database.py
+++ slack_bot/metric_watch_interface/database.py
@@ -1,12 +1,8 @@
 from __future__ import annotations
 
-from os import environ as env
-
 from dotenv import load_dotenv
-from infi.clickhouse_orm import Database, DateTimeField, Model, StringField
+from infi.clickhouse_orm import DateTimeField, Model, StringField
 from infi.clickhouse_orm.engines import MergeTree
-
-from slack_bot.metric_watch_interface.constants import METRIC_WATCH_DB
 
 load_dotenv(override=True)
 
@@ -23,14 +19,3 @@ class Subscriptions(Model):
     subscribed_at = DateTimeField()
 
     engine = MergeTree(partition_key=("user",), order_by=("subscribed_at",))
-
-
-db = Database(
-    db_name=METRIC_WATCH_DB,
-    db_url=str(env.get("CLICKHOUSE_HOST_URL")),
-    username=env.get("CLICKHOUSE_USER"),
-    password=env.get("CLICKHOUSE_PASSWORD"),
-)
-# Create tables if not exist
-db.create_table(Metrics)
-db.create_table(Subscriptions)
